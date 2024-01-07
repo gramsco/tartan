74646d0395c0d3ecdecf9fee6bab79dae9fc38f8
gramsco <antoningauth@gmail.com>
2024-01-07 16:14:33 +0100

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tartan
+
+The script generates a random "tartan like" gif composed of 10 frames. 
+
+![image](gifs/tartan.gif)
+
---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use std::fs::File;
+
+use image::{ImageBuffer, Rgb};
+use rand::Rng;
+
+fn create_random() -> [bool;800]{
+    let mut arr:[bool; 800] = [false; 800];
+    let mut rng = rand::thread_rng();
+    for _ in 0..300 {
+        let r = rng.gen_range(0..arr.len());
+        arr[r] = true
+    }
+    arr
+}
+
+fn write_buffer() -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    let imgx = 800;
+    let imgy = 800;
+
+    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+
+    let r_x = create_random();
+    let r_y = create_random();
+
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut()  {
+        
+        let r = match r_x[x as usize] {
+            true => 255,
+            _ => 0
+        };
+        let b = match r_y[y as usize] {
+            true => 255,
+            _ => 0
+        };
+        *pixel = image::Rgb([r, 0_u8,  b]);
+    }
+        imgbuf
+
+}
+
+fn main() {
+    let mut image = File::create("gifs/tartan.gif").unwrap();
+    let mut encoder = gif::Encoder::new(&mut image, 800,800, &[]).unwrap();
+
+    for _ in 0..10 {
+        let img = write_buffer();
+        let frame = gif::Frame::from_rgb(800,800,&img);
+        let _ = encoder.write_frame(&frame);
+    }
+    println!("Done.")
+}
